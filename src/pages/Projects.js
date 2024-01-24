53f63c8fd6e466069ddd449e9c335ba53caeb5c8
Alexander Bengtsson <palexanderbengtsson@gmail.com>
2024-01-24 10:39:33 +0100

    Left layout on homepage

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,66 +6,61 @@ export default function Projects() {
     <div>
       <div className="flex-grow text-neutral-200 bg-slate-800 py-8 sm:py-10">
         <div id="projects" className="container mx-auto min-h-dvh justify-center body-font">
-          <div className="container px-5 mx-auto text-center lg:px-40">
-            <div className="text-lg">
-              <h2 className="sm:text-xl mt-5">
-                Hi, I'm Alexander Bengtsson
-                <p> </p>
-                <a
-                  href="https://www.github.com/panabana/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-sky-600 hover:underline">
-                  GitHub: (@Panabana)
-                </a>
-                <br className="hidden lg:inline-block" />
+          <div className="container px-5 mx-auto text-left lg:px-40">
+            <div>
+              <h2 className="text-xl mt-5">
+                Hi, I'm Alexander Bengtsson. Thanks for checking out this website.
               </h2>
-              <h3 className="p-2  mb-6">
-                Thanks for checking out this website
-              </h3>
+              <a
+                href="https://www.github.com/panabana/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-xl text-sky-600 hover:underline">
+                GitHub: (@Panabana)
+              </a>
+              <br className="hidden lg:inline-block" />
             </div>
-            <div className="font-medium font-mono text-[2rem] md:text-[3rem] p-4 mt-32">
+            <div className="font-medium font-mono text-[2rem] md:text-[3rem] mt-10 mb-4">
               <h3>
                 My projects:
               </h3>
             </div>
             <div className="grid grid-rows-2 grid-cols-1 ">
-              <div className="container mx-auto place-content-center">
-                <p className="text-lg sm:text-xl font-medium p-6 italic">
+              <div className="container w-[60%] mb-8">
+                <h4 className="text-lg sm:text-xl font-medium mt-4 italic">
                   The Inventory Management System
-                </p>
-                <p className="text-base font-normal p-4">
-                  Two different frontend apps in different programming languages, connected to the same backend database.
+                </h4>
+                <p className="text-base font-normal mt-4 mb-4">
+                  Built to maintain and organize a given warehouse's inventory.
+                  <br />
+                  Two frontend apps were built in different programming languages. Java was used for the backend. SQL Server was used for the database.
                   <br />
                   <a
                     className="text-sky-600 hover:underline font-bold"
                     href="https://github.com/Panabana/sfc-inventory-management-system-web"
                     target="_blank"
                     rel="noopener noreferrer"
-                  >1 web app built with JS and JQuery (further details) </a>
-                  and
+                  >The web app built with JS and JQuery (further details) </a>
                   <br />
                   <a
                     className="text-sky-600 hover:underline font-bold"
                     href="https://github.com/Panabana/sfc-inventory-management-system-app"
                     target="_blank"
                     rel="noopener noreferrer"
-                  >1 desktop app built with C# (further details).</a>
-                  <br />
-                  I built these two apps together with a team of five people.
-                  <br />
-                  We built our own internal tooling and built the apps so that data would persist between our client side and Java server.
+                  >The desktop app built with C# (further details)</a>
+                  <br /><br />
+                  I built these two apps together with a team of five people. We built our own internal tooling and built the apps so that data would persist between our client side and Java server.
                 </p>
-                <div className="container mx-auto w-[95%] md:w-[60%]">
+                <div className="container w-[95%] md:w-[100%]">
                   <img
                     src="./screenshots/startScreenIMS.png"
                     alt="Screenshot of the Inventory Management System start screen" />
                 </div>
               </div>
-              <div>
-                <p className="text-lg sm:text-xl font-medium italic mt-28 p-6">
+              <div className="container w-[60%] mb-0">
+                <h4 className="text-lg sm:text-xl font-medium mt-6 italic">
                   Personal Website
-                </p>
+                </h4>
                 <a
                   href="https://github.com/Panabana/panabana-portfolio-2024"
                   target="_blank"
@@ -73,10 +68,10 @@ export default function Projects() {
                   className="text-sky-600 hover:underline">
                   Link to GitHub repo.
                 </a>
-                <p className="text-base font-normal p-4">
+                <p className="text-base font-normal mt-4">
                   This website, of course.
                   <br />
-                  Built with React, Tailwind and React Router.
+                  Built with React, Tailwind, Node.js and React Router.
                 </p>
               </div>
             </div>
